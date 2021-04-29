390ee3d307fcde231276329ff971067319061c69
mohiuddinmohi <mohiuddin0240@gmail.com>
2021-04-30 01:13:27 +0600

    functionality added

--- split-landing-page/script.js
+++ split-landing-page/script.js
@@ -1,19 +1,17 @@
-const boxes = document.querySelectorAll('.box')
+const left = document.querySelector('.left')
+const right = document.querySelector('.right')
+const container = document.querySelector('.container')
 
-window.addEventListener('scroll', checkBoxes)
+left.addEventListener('mouseenter', function() {
+  container.classList.add('hover-left')
+})
+left.addEventListener('mouseleave', function() {
+  container.classList.remove('hover-left')
+})
 
-checkBoxes()
-
-function checkBoxes() {
-  const triggerBottom = window.innerHeight / 5 * 4
-
-  boxes.forEach(box => {
-    const boxTop = box.getBoundingClientRect().top
-
-    if (boxTop < triggerBottom) {
-      box.classList.add('show')
-    } else {
-      box.classList.remove('show')
-    }
-  })
-}
+right.addEventListener('mouseenter', function() {
+  container.classList.add('hover-right')
+})
+right.addEventListener('mouseleave', function() {
+  container.classList.remove('hover-right')
+})
